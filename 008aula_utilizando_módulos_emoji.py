191b9f72d1972aa944fe4114bdd0a7642ebce45e
luiz-D-silva <luiz.adriano.99@hotmail.com>
2023-01-02 23:52:39 -0400

    Create 008aula_utilizando_módulos_emoji.py

---
+++ 008aula_utilizando_módulos_emoji.py
@@ -0,0 +1,14 @@
+# Nessa aula, vamos aprender como utilizar módulos em Python utilizando os
+# comandos import e from/import no Python. Veja como carregar bibliotecas de
+# funções e utilizar vários recursos adicionais nos seus programas utilizando
+# módulos built-in e módulos externos, oferecidos no Pypi.
+
+# código do aluno, o do guanabara não rodou, ainda desconheço o motivo
+import emoji
+
+print(emoji.emojize('Olá, mundo :innocent:', language='alias'))
+
+# o código do guanabara
+
+# import emojiS
+# print(emoji.emojize('Olá, mundo :innocent:', use_aliases=True))
