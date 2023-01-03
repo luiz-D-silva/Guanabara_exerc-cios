cfdea4e660a75e511d59b86bcea6afa7ed79bdd0
luiz-D-silva <luiz.adriano.99@hotmail.com>
2023-01-03 11:00:20 -0400

    Update 018.seno, cosseno, tangente.py

--- 018.seno, cosseno, tangente.py
+++ 018.seno, cosseno, tangente.py
@@ -8,4 +8,9 @@ seno = math.sin(math.radians(angulo))
 # os valores de seno, cosseno e tangente saõ dados em radianos
 # por isso necessita-se usr a função 'math.radians' para converter de
 # radianos para graus centrígrados
-print('O angulo de {} tem o SENO de {:.2F}.'.format(angulo, seno))
+cosseno = math.cos(math.radians(angulo))
+tangente = math.tan(math.radians(angulo))
+
+print('Para o ângulo de {}°'.format(angulo))
+print(' Temos:\n O SENO de {:.2}\n O COSSENO de {:.2}\n A TANGENTE de {:.2}'.
+      format(seno, cosseno, tangente))
