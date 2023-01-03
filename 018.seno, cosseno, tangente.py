62f5d9a3309dedcb768404c3802acd9ce1969333
luiz-D-silva <luiz.adriano.99@hotmail.com>
2023-01-03 11:03:17 -0400

    Update 018.seno, cosseno, tangente.py

--- 018.seno, cosseno, tangente.py
+++ 018.seno, cosseno, tangente.py
@@ -1,15 +1,15 @@
 '''Faça um programa que leia um ângulo qualquer e mostre
 na tela o valor do seno, cosseno e tangente desse ângulo.'''
 
-import math
+from math import cos, radians, sin, tan
 
-angulo = float(input('Digite o ângulo que você deseja: '))
-seno = math.sin(math.radians(angulo))
+angulo = float(input('Digite um ângulo:'))
+seno = sin(radians(angulo))
 # os valores de seno, cosseno e tangente saõ dados em radianos
 # por isso necessita-se usr a função 'math.radians' para converter de
 # radianos para graus centrígrados
-cosseno = math.cos(math.radians(angulo))
-tangente = math.tan(math.radians(angulo))
+cosseno = cos(radians(angulo))
+tangente = tan(radians(angulo))
 
 print('Para o ângulo de {}°'.format(angulo))
 print(' Temos:\n O SENO de {:.2}\n O COSSENO de {:.2}\n A TANGENTE de {:.2}'.
