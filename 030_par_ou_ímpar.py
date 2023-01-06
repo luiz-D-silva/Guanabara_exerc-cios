0355fa9e2aaed4ed4c16d934f28c36eb03718b13
luiz-D-silva <luiz.adriano.99@hotmail.com>
2023-01-06 10:26:05 -0400

    Create 030_par_ou_ímpar.py

---
+++ 030_par_ou_ímpar.py
@@ -0,0 +1,9 @@
+'''O símbolo % em Python é chamado de operador modulo. Ele retorna o resto da
+divisão do operando da esquerda pelo operando da direita. Ele é usado para
+obter o resto de um problema de divisão.'''
+
+num = int(input('Digite um número qualquer: '))
+if num % 2 == 0:
+    print('O número digitado é PAR!')
+else:
+    print('O número digitado é IMPAR!')
