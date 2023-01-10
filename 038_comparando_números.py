e19cbd54edcdd6482ded0145394e2bdbad2440fc
luiz-D-silva <luiz.adriano.99@hotmail.com>
2023-01-10 09:44:31 -0400

    Create 038_comparando_números.py

---
+++ 038_comparando_números.py
@@ -0,0 +1,8 @@
+num = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+if num > num2:
+    print('O valor {} é maior'.format(num))
+elif num < num2:
+    print('O valor {} é maior'.format(num2))
+else:
+    print('Os valores são iguais.')
