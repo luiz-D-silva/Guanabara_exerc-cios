977dc281dd5015dd7a465af8ecd91687ddcbb353
luiz-D-silva <luiz.adriano.99@hotmail.com>
2023-01-30 16:14:20 -0400

    Update 053_Detector_de_palindromos.py

--- 053_Detector_de_palindromos.py
+++ 053_Detector_de_palindromos.py
@@ -1,9 +1,23 @@
-frase = str(input('Digite uma frase: ')).strip().upper()
+'''frase = str(input('Digite uma frase: ')).strip().upper()
 palavras = frase.split()
 junto = ''.join(palavras)
 inverso = ''
 for letra in range(len(junto) - 1, -1, -1):
     inverso += junto[letra]
+print('O inverso de {} é {}'.format(junto, inverso))
+if inverso == junto:
+    print('A frase {} \033[32mÉ UM PALÍNDROMO!.'.format(frase))
+else:
+    print('A frase digitada não é um palíndromo')'''
+
+############################### versão sem o FOR##########################
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = junto[::-1]
+# o inverso será igual ao junto, começando no início e terminando no fim
+# de trás pra frente. Dois pontos seguidos pegam a frase do início ao fim
+# e o -1 indica que a contagem deverá ser decrescente.
 print('O inverso de {} é {}'.format(junto, inverso))
 if inverso == junto:
     print('A frase {} \033[32mÉ UM PALÍNDROMO!.'.format(frase))
